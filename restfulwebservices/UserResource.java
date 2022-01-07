8021225f7e1d7a710cd0ed565e0e2c7a59a1b403
YachendraKumar <91861548+YachendraKumar@users.noreply.github.com>
2022-01-07 21:08:05 +0530

    Updated delete user

--- restfulwebservices/UserResource.java
+++ restfulwebservices/UserResource.java
@@ -3,8 +3,11 @@ package yachendra.com.restfulwebservices;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,13 +16,21 @@ public class UserResource {
 	@Autowired
 	private UserDaoService service;
 	
-	@GetMapping("/users")
+	@GetMapping("/users") 
 	public List<User> retrieveAllUsers(){
            return service.findAll();
 	}        
-           @GetMapping("/users/{id}")
+           @DeleteMapping("/users/{id}")
            public User retrieveUser(@PathVariable int id) {
-        	   return service.findOne(id);
+        	   User user =  service.deleteById(id);
+        	if(user==null) {
+        		throw new UserNotFoundException("id-"+id);
+        	}
+        	   return user;
            }
- 
+           
+        @PostMapping("/users")
+        public void createUser(@RequestBody User user) {
+        	User savedUser = service.save(user);
+        }
 }
